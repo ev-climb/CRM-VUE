b24b52538c27395fbfcf566ad8b61b5f4947c0d4
Евгений <evseev.develop@gmail.com>
2023-06-09 13:42:43 +0300

    добавил валидацию форм

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
+import messagePlagin from './utils/message.plagin'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
 
+Vue.use(messagePlagin)
 Vue.filter('date', dateFilter)
+Vue.use(Vuelidate)
 
 new Vue({
   router,

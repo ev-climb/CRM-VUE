eb32eefa7e407de1d6837fff6d65b3ca986cf10a
Evgenii Evseev <ev-ev89@mail.ru>
2023-06-11 18:00:14 +0300

    Начал настройку firebase

--- src/main.js
+++ src/main.js
@@ -7,11 +7,30 @@ import dateFilter from './filters/date.filter'
 import messagePlagin from './utils/message.plagin'
 import 'materialize-css/dist/js/materialize.min'
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(messagePlagin)
 Vue.filter('date', dateFilter)
 Vue.use(Vuelidate)
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+firebase.initializeApp({
+    apiKey: "AIzaSyCHnAYOrH5cNL191lYGDxt5sug9y9wNzcg",
+    authDomain: "crm-vue-59ddb.firebaseapp.com",
+    projectId: "crm-vue-59ddb",
+    storageBucket: "crm-vue-59ddb.appspot.com",
+    messagingSenderId: "44396286852",
+    appId: "1:44396286852:web:fd9884a48b6582776103d1",
+    measurementId: "G-R2JXZKQN0K"
+  })
 
 new Vue({
   router,

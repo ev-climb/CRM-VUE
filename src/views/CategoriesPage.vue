c543a50831b0bbba546a3b4123b41cbb1e63d8ff
Евгений <evseev.develop@gmail.com>
2023-06-22 17:07:34 +0300

    закончен 11 урок

--- src/views/CategoriesPage.vue
+++ src/views/CategoriesPage.vue
@@ -4,9 +4,15 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class="row">
+      <Loader v-if="loading" />
+      <div v-else class="row">
         <CategoryCreate @created="addNewCategory" />
-        <CategoryEdit />
+        <CategoryEdit
+          v-if="categories.length"
+          @updated="updateCategories"
+          :categories="categories"
+          :key="categories.length + updateCount" />
+        <p v-else class="center">Категорий пока нет</p>
       </div>
     </section>
   </div>
@@ -19,7 +25,13 @@
     name: 'categories',
     data: () => ({
       categories: [],
+      loading: true,
+      updateCount: 0,
     }),
+    async mounted() {
+      this.categories = await this.$store.dispatch('fetchCategories');
+      this.loading = false;
+    },
     components: {
       CategoryCreate,
       CategoryEdit,
@@ -27,6 +39,12 @@
     methods: {
       addNewCategory(category) {
         this.categories.push(category);
+      },
+      updateCategories(category) {
+        const idx = this.categories.findIndex((c) => c.id === category.id);
+        this.categories[idx].title = category.title;
+        this.categories[idx].limit = category.limit;
+        this.updateCount++;
       },
     },
   };

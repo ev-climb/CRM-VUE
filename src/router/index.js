b11329fad0633674942a858035afd74ef52db981
Евгений <evseev.develop@gmail.com>
2023-06-27 16:12:09 +0300

    закончил урок 16

--- src/router/index.js
+++ src/router/index.js
@@ -1,69 +1,81 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import firebase from 'firebase/app';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    meta: { layout: "main" },
-    component: () => import("@/views/HomePage.vue"),
+    path: '/',
+    name: 'home',
+    meta: { layout: 'main', auth: true },
+    component: () => import('@/views/HomePage.vue'),
   },
   {
-    path: "/login",
-    name: "login",
-    meta: { layout: "empty" },
-    component: () => import("@/views/LoginPage.vue"),
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('@/views/LoginPage.vue'),
   },
   {
-    path: "/register",
-    name: "register",
-    meta: { layout: "empty" },
-    component: () => import("@/views/RegisterPage.vue"),
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('@/views/RegisterPage.vue'),
   },
   {
-    path: "/categories",
-    name: "categories",
-    meta: { layout: "main" },
-    component: () => import("@/views/CategoriesPage.vue"),
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main', auth: true },
+    component: () => import('@/views/CategoriesPage.vue'),
   },
   {
-    path: "/ditail",
-    name: "ditail",
-    meta: { layout: "main" },
-    component: () => import("@/views/DitailRecordPage.vue"),
+    path: '/ditail',
+    name: 'ditail',
+    meta: { layout: 'main', auth: true },
+    component: () => import('@/views/DitailRecordPage.vue'),
   },
   {
-    path: "/history",
-    name: "history",
-    meta: { layout: "main" },
-    component: () => import("@/views/HistoryPage.vue"),
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main', auth: true },
+    component: () => import('@/views/HistoryPage.vue'),
   },
   {
-    path: "/planning",
-    name: "planning",
-    meta: { layout: "main" },
-    component: () => import("@/views/PlanningPage.vue"),
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main', auth: true },
+    component: () => import('@/views/PlanningPage.vue'),
   },
   {
-    path: "/profile",
-    name: "profile",
-    meta: { layout: "main" },
-    component: () => import("@/views/ProfilePage.vue"),
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main', auth: true },
+    component: () => import('@/views/ProfilePage.vue'),
   },
   {
-    path: "/record",
-    name: "record",
-    meta: { layout: "main" },
-    component: () => import("@/views/RecordPage.vue"),
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main', auth: true },
+    component: () => import('@/views/RecordPage.vue'),
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requireAuth = to.matched.some((record) => record.meta.auth);
+
+  if (requireAuth && !currentUser) {
+    next('/login?message=login');
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
   logout: 'Вы вышли из системы',
+  login: 'Для начала войдите в систему',
   'auth/user-not-found': 'Пользователя с такими данными не существует',
   'auth/wrong-password': 'Пароль неверный',
   'auth/email-already-in-use': 'Пользователь с таким e-mail уже существует',
